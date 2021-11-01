6e88f2f078c90497071eb2d32e4c727e71e883f2
katerina-tziala <katerina.tziala@gmail.com>
2021-11-01 22:23:47 +0100

    feat: add default get endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,23 @@ import cors from 'cors';
 import routes from './routes/index';
 
 // .env, cross platoform, node env
+// Add in logging to record when images are processed or accessed
+// Write relevant unit tests with Jasmine and SuperTest to improve code quality and refactoring
+
+// Test script runs and all tests created pass.
+// There is at least 1 test per endpoint and at least one test for image processing.
 const port = 3000;
+const startedAt = new Date().toUTCString();
 
 const app = express();
 
 app.use(cors());
 
 app.use('/api', routes);
+
+app.get('/', (_, res) => {
+  res.status(200).send(`Server started at ${startedAt}`);
+});
 
 app.listen(port, () => {
   // prettier-ignore
